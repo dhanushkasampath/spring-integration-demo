76a698660de016a5970453ba251da8fef6b065b0
dhanushkasampath <dhanushka.2018410@iit.ac.lk>
2024-09-23 10:48:38 +0530

    first

---
+++ src/main/java/com/learn/spring_integration_demo/SpringIntegrationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.learn.spring_integration_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntegrationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntegrationDemoApplication.class, args);
+	}
+
+}
